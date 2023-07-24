cc07d32c650bf49b9f68a99663fab6fb53ab8b1e
Huseyin <huseyin.bbcn@outlook.com>
2023-07-24 22:08:55 +0300

    layout eklendi

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <strong>Copyright &copy; {{date('Y')}}  <a target="_blank" href="https://seytim.org">SEYTİM</a>.</strong>
+    </div>
+    Tüm hakları saklıdır.
+</footer>
